1a5f4fb8bda65870d74bd6c367af42cc0c7309cd
Mallne <info@mallne.cloud>
2025-10-16 21:00:26 +0200

    Modularize Esri Adapter pt 3

--- src/commonMain/kotlin/cloud/mallne/dicentra/areaassist/model/parcel/domain/ParcelCrateDomain.kt
+++ src/commonMain/kotlin/cloud/mallne/dicentra/areaassist/model/parcel/domain/ParcelCrateDomain.kt
@@ -1,10 +1,19 @@
 package cloud.mallne.dicentra.areaassist.model.parcel.domain
 
-interface ParcelCrateDomain<out Parcel : ParcelDomain, out Landmark : LandmarkDomain, out Task : TaskDomain, out Note : NoteDomain, out LandUsage : LandUsageDomain, out ParcelProperty : ParcelPropertyDomain> {
+interface ParcelCrateDomain<Parcel : ParcelDomain, Landmark : LandmarkDomain, Task : TaskDomain, Note : NoteDomain, LandUsage : LandUsageDomain, ParcelProperty : ParcelPropertyDomain> {
     val parcel: Parcel
     val landMarks: List<Landmark>
     val tasks: List<Task>
     val notes: List<Note>
     val landUsages: List<LandUsage>
     val properties: List<ParcelProperty>
+
+    fun clone(
+        parcel: Parcel = this.parcel,
+        landMarks: List<Landmark> = this.landMarks,
+        tasks: List<Task> = this.tasks,
+        notes: List<Note> = this.notes,
+        landUsages: List<LandUsage> = this.landUsages,
+        properties: List<ParcelProperty> = this.properties,
+    ): ParcelCrateDomain<Parcel, Landmark, Task, Note, LandUsage, ParcelProperty>
 }
--- src/commonMain/kotlin/cloud/mallne/dicentra/areaassist/model/parcel/domain/data/ParcelCrateEntity.kt
+++ src/commonMain/kotlin/cloud/mallne/dicentra/areaassist/model/parcel/domain/data/ParcelCrateEntity.kt
@@ -11,4 +11,20 @@ data class ParcelCrateEntity(
     override var notes: List<NoteEntity> = emptyList(),
     override var landUsages: List<LandUsageEntity> = emptyList(),
     override var properties: List<ParcelPropertyEntity> = emptyList(),
-) : ParcelCrateDomain<ParcelEntity, LandmarkEntity, TaskEntity, NoteEntity, LandUsageEntity, ParcelPropertyEntity>
+) : ParcelCrateDomain<ParcelEntity, LandmarkEntity, TaskEntity, NoteEntity, LandUsageEntity, ParcelPropertyEntity> {
+    override fun clone(
+        parcel: ParcelEntity,
+        landMarks: List<LandmarkEntity>,
+        tasks: List<TaskEntity>,
+        notes: List<NoteEntity>,
+        landUsages: List<LandUsageEntity>,
+        properties: List<ParcelPropertyEntity>
+    ): ParcelCrateEntity = ParcelCrateEntity(
+        parcel,
+        landMarks,
+        tasks,
+        notes,
+        landUsages,
+        properties,
+    )
+}
