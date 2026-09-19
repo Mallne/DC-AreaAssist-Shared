0b18ded47913d12043860381ffa9801855363852
mallne <info@mallne.cloud>
2026-09-19 10:10:55 +0200

    fix: proper gradle wrapper, conditional signing, updated workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ versionCatalogUpdate {
 }
 
 group = "cloud.mallne.dicentra.areaassist"
-version = "1.0.0-SNAPSHOT"
+version = project.findProperty("VERSION_NAME") ?: "1.0.0-SNAPSHOT"
 
 kotlin {
     jvm()
@@ -118,7 +118,9 @@ mavenPublishing {
         }
     }
 
-    signAllPublications()
+    if (project.findProperty("dc.signingKey") != null) {
+        signAllPublications()
+    }
 
-    coordinates(group.toString(), project.name, version.toString())
+    coordinates(group.toString(), project.name)
 }
